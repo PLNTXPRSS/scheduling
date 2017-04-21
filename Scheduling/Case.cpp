aee18a263ecf4f1fdfba8e24d4a821d0b384a207
Jesús Castro <51v4n@openmailbox.org>
2017-04-21 14:17:53 -0500

    fix #1
    fixed case problems
    
    Misa Guzmán: @strogolandia
    Jesús Castro: @51v4n
    [ticket: fix]

--- Scheduling/Case.cpp
+++ Scheduling/Case.cpp
@@ -8,88 +8,70 @@
 #include "roundrobin.cpp"
 
 int main(int argc, char const *argv[])
- {
-   int opcion;
+{
+    int opcion;
 
-   do{
-   puts("¿Que modalidad desea utilizar?\n1: FIFO\n2: SJF\n3: Round Robin\n4: Prioridad\n0: Salir\n\nOpción:");
-   scanf("%i", &opcion);
+    do {
+    puts("¿Que modalidad desea utilizar?\n1: FIFO\n2: SJF\n3: Round Robin\n4: Prioridad\n0: Salir\n\nOpción:");
+    scanf("%i", &opcion);
 
-   switch(opcion)
-   {
-
-      //FIFO
-      case 1 :
-
-         break;
-
-      //SJF
-      case 2 :
-
-      /* Round Robin */
-      case 3:
-
-        break;
-      /* Prioridad */
-      case 4:
-
-        break;
-
-      /* Salir */
-        case 0:
-
-          return 0;
+    switch(opcion)
     {
-    srand(time(NULL));
-  int dato, cantdatos, prioridad, quantum;
-  SortList *lista = new SortList();
+        //FIFO
+        case 1 :
 
-  puts("Introduzca la cantidad de datos que desee generar para arreglar: ");
-  scanf("%i",&cantdatos);
+        break;
 
-  int Milistado[cantdatos];
-  int listaprioridad[cantdatos];
+        //SJF
+        case 2 :
+        {
+            srand(time(NULL));
+            int dato, cantdatos, prioridad, quantum;
+            SortList *lista = new SortList();
 
-  for (int i = 0; i < cantdatos; ++i)
-  {
-    dato = rand() % 100+1;
-    Milistado[i] = dato;
-    bool Nnumber = lista->insert(dato);
-    //printf("Posición %i: %i - Insertado: %i\n", i, dato, Nnumber);
-  }
+            puts("Introduzca la cantidad de datos que desee generar para arreglar: ");
+            scanf("%i",&cantdatos);
 
-  for (int i = 0; i < cantdatos; ++i)
-  {
-    dato = rand() % 10+1;
-    listaprioridad[i] = dato;
-  }
+            int Milistado[cantdatos];
+            int listaprioridad[cantdatos];
 
-    printf("\nListado: \n");
-  for (int i = 0; i < cantdatos; ++i)
-  {
-  printf("Tiempo %i: %i -- Prioridad: %i \n", i, Milistado[i], listaprioridad[i]);
+            for (int i = 0; i < cantdatos; ++i) {
+                dato = rand() % 100+1;
+                Milistado[i] = dato;
+                bool Nnumber = lista->insert(dato);
+                //printf("Posición %i: %i - Insertado: %i\n", i, dato, Nnumber);
+            }
+
+            for (int i = 0; i < cantdatos; ++i) {
+                dato = rand() % 10+1;
+                listaprioridad[i] = dato;
+            }
+
+            printf("\nListado: \n");
+  
+            for (int i = 0; i < cantdatos; ++i) {
+                printf("Tiempo %i: %i -- Prioridad: %i \n", i, Milistado[i], listaprioridad[i]);
+            }
+
+            puts("\nResult: \nTiempos  Prioridad");
+            lista->FinalOrder();
+            puts("");
+
+            delete lista;
+        } break;
+
+        // Round Robin
+        case 3:
+
+            break;
+
+        /* Priority */    
+        case 4:
+            priority::main();
+            break;
     }
 
-  puts("\nResult: \nTiempos  Prioridad");
-  lista->FinalOrder();
-  puts("");
+   } while (opcion != 0);
 
-  delete lista;
-    }
-         break;
-
-      //Round Robin
-      case 3 :
-
-         break;
-
-      //Prioridad
-      case 4 :
-
-         break;
-
-   }
-   }while (opcion !=0);
-
-   return 0;
+    return 0;
 }
